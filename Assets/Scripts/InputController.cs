b836a69fa948fe013ddaa7d9bf855404c6d5a73e
ThomasDavenel <thomas.davenel@outlook.com>
2021-11-15 09:41:35 +0100

    Animation : il peut aller a droite et a gauche et tourner (mais il reste certain Bug)

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -2,41 +2,76 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputController : MonoBehaviour
 {
     public GameObject Character;
     public Animator RobotAnimator;
     private float dt;
+    private Orientation m_orientation;
+    enum Orientation
+    {
+        Right,
+        Left,
+    }
+
+    private void Start()
+    {
+        m_orientation = Orientation.Right;
+    }
 
     void Update()
     {
+        //Move
         if (Input.GetButtonDown("Move_Right"))
         {
-            Debug.Log("Walk");
-            RobotAnimator.SetTrigger("Walk");
-        }
-        if (Input.GetButton("Move_Right"))
-        {
-            dt += Time.deltaTime;
-            Move(dt);
-        }
-        else
-        {
-            RobotAnimator.ResetTrigger("Walk");
-            dt = 0;
+            if(m_orientation == Orientation.Left)
+            {
+                RobotAnimator.SetTrigger("WalkR");
+                m_orientation = Orientation.Right;
+            }
+            else
+            {
+                RobotAnimator.SetTrigger("WalkR");
+            }
         }
 
         if (Input.GetButtonDown("Move_Left"))
         {
-            Debug.Log("Turn");
-            RobotAnimator.SetTrigger("Turn");
+            if (m_orientation == Orientation.Right)
+            {
+                RobotAnimator.SetTrigger("WalkL");
+                m_orientation = Orientation.Left;
+            }
+            else
+            {
+                RobotAnimator.SetTrigger("WalkL");
+            }
+        }
+        if (Input.GetButton("Move_Right") || Input.GetButton("Move_Left"))
+        {
+            dt += Time.deltaTime;
+            Move(dt);
         }
 
+        //STOP
         if (Input.GetButtonUp("Move_Right"))
         {
-            Debug.Log("Stop");
-            RobotAnimator.SetTrigger("Stop");
+            dt = 0;
+            RobotAnimator.SetTrigger("StopR");
+        }
+        if (Input.GetButtonUp("Move_Left"))
+        {
+            dt = 0;
+            RobotAnimator.SetTrigger("StopL");
+
+        }
+
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            RobotAnimator.SetTrigger("Jump");
         }
     }
 
@@ -51,8 +86,8 @@ public class InputController : MonoBehaviour
         {
             dtsqrt = (float)Math.Pow(dt, 2);
         }
-        float t = Mathf.Clamp(dtsqrt, 0, 5)/50;
-        Debug.Log(dt + "-->"+ dtsqrt+"-->" + t);
+        float t = Mathf.Clamp(dtsqrt, 0, 2.5f)/50;
+        if (m_orientation == Orientation.Left) t = -t;
         Character.transform.position += t * Character.transform.forward;
     }
 }
