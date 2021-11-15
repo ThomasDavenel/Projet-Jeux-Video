b051f1127d0611ef950df8eb0daa2444b5ed0547
elifra <mael.levot@gmail.com>
2021-11-15 09:36:15 +0100

    menu fini

--- Assets/Scripts/SelectMenu.cs
+++ Assets/Scripts/SelectMenu.cs
@@ -1,59 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class SelectMenu : MonoBehaviour
+public class SelectMenu : MonoBehaviour, IPointerEnterHandler
 {
-    private Vector3 mousePos;
-    public List<GameObject> buttonPos;
-    public float buttonWidth;
-    public float buttonHeight;
+    public GameObject bandeau;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        bandeau.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = Input.mousePosition;
+       
+    }
 
-        //new game button
-        if (mousePos.x > buttonPos[0].transform.position.x - buttonWidth && mousePos.x < buttonPos[0].transform.position.x + buttonWidth)
-        {
-            if (mousePos.y > buttonPos[0].transform.position.y - buttonHeight && mousePos.y < buttonPos[0].transform.position.y + buttonHeight)
-            {
-                transform.position = buttonPos[0].transform.position;
-            }
-        }
-
-        //load game button
-        if (mousePos.x > buttonPos[1].transform.position.x - buttonWidth && mousePos.x < buttonPos[1].transform.position.x + buttonWidth)
-        {
-            if (mousePos.y > buttonPos[1].transform.position.y - buttonHeight && mousePos.y < buttonPos[1].transform.position.y + buttonHeight)
-            {
-                transform.position = buttonPos[1].transform.position;
-            }
-        }
-
-        //controls button
-        if (mousePos.x > buttonPos[2].transform.position.x - buttonWidth && mousePos.x < buttonPos[2].transform.position.x + buttonWidth)
-        {
-            if (mousePos.y > buttonPos[2].transform.position.y - buttonHeight && mousePos.y < buttonPos[2].transform.position.y + buttonHeight)
-            {
-                transform.position = buttonPos[2].transform.position;
-            }
-        }
-
-        //quit button
-        if (mousePos.x > buttonPos[3].transform.position.x - buttonWidth && mousePos.x < buttonPos[3].transform.position.x + buttonWidth)
-        {
-            if (mousePos.y > buttonPos[3].transform.position.y - buttonHeight && mousePos.y < buttonPos[3].transform.position.y + buttonHeight)
-            {
-                transform.position = buttonPos[3].transform.position;
-            }
-        }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        bandeau.SetActive(true);
+        bandeau.transform.position = transform.position;
     }
 }
