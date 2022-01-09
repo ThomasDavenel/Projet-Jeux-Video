b3008ab8d4daca5524d267cac201298c949a0b7e
Guillaume ROBERT <gr99@orange.fr>
2022-01-09 23:20:27 +0100

    Modification script bouton rouge pour adaptation aux énigmes

--- Assets/Scripts/Button_on_hit.cs
+++ Assets/Scripts/Button_on_hit.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class Button_on_hit : MonoBehaviour
 {
-    public GameObject ObjectToAppear;
-    public Texture NewTexture;
+    public List<GameObject> objectsToToggle;
+    public Texture textureActive;
+    public Texture textureInactive;
+    public float timeActive;
 
     private Material m_Material;
+    private bool active;
+    private float timeSinceActive;
 
     void Start()
     {
         m_Material = GetComponent<Renderer>().material;
+        timeSinceActive = 0f;
+        active = false;
     }
 
-        private void OnTriggerEnter(Collider collid)
+    private void OnTriggerEnter(Collider collid)
     {
-        ObjectToAppear.SetActive(true);
-        m_Material.SetTexture("_MainTex", NewTexture);
+        foreach (GameObject obj in objectsToToggle)
+        {
+            if (obj.activeSelf)
+            {
+                obj.SetActive(false);
+            }
+            else { obj.SetActive(true); }  
+        }
+        m_Material.SetTexture("_MainTex", textureActive);
+        active = true;
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            timeSinceActive += Time.deltaTime;
+            if(timeSinceActive >= timeActive)
+            {
+                m_Material.SetTexture("_MainTex", textureInactive);
+                active = false;
+                timeSinceActive=0f;
+            }
+        }
     }
 }
