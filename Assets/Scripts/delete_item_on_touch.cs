5879a9a314e1cded81592a5da371065c4b34efa4
Guillaume ROBERT <gr99@orange.fr>
2022-01-10 21:17:19 +0100

    Ajouts de sons pickup pistol et victoire

--- Assets/Scripts/delete_item_on_touch.cs
+++ Assets/Scripts/delete_item_on_touch.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class delete_item_on_touch : MonoBehaviour
 {
+    public GameObject pickup_sound;
+
     private void OnTriggerEnter(Collider collid)
     {
+        pickup_sound.SetActive(true);
         Destroy(gameObject);
     }
 }
